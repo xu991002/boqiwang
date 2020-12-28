3596a33ec98adb4801f0743f331c672fc27cff55
jiangxu <522314256@qq.com>
2020-12-28 16:59:37 +0800

    提交首页

--- app.js
+++ app.js
@@ -19,9 +19,108 @@ app.use((req, res, next) => {
 });
 
 // 首页
-app.get('/index', function (req, res) {
-    res.send('hello world')
+app.get('/index1', (req, res)=> {
+  connection.query(`select * from index1 where type_id=1`,function(err,rows){
+   res.send({
+     data:rows,
+     status:200
+   })
   })
+  })
+  // 首页 type_id=2
+  app.get('/index2', (req, res)=> {
+    connection.query(`select * from index1 where type_id=2`,function(err,rows){
+     res.send({
+       data:rows,
+       status:200
+     })
+    })
+    })
+    // 首页 type_id=3
+  app.get('/index3', (req, res)=> {
+    connection.query(`select * from index1 where type_id=3`,function(err,rows){
+     res.send({
+       data:rows,
+       status:200
+     })
+    })
+    })
+    // 首页 type_id=4
+    app.get('/index4', (req, res)=> {
+      connection.query(`select * from index1 where type_id=4`,function(err,rows){
+       res.send({
+         data:rows,
+         status:200
+       })
+      })
+      })
+      // 首页 type_id=5
+    app.get('/index5', (req, res)=> {
+      connection.query(`select * from index1 where type_id=5`,function(err,rows){
+       res.send({
+         data:rows,
+         status:200
+       })
+      })
+      })
+      // 首页 type_id=6
+    app.get('/index6', (req, res)=> {
+      connection.query(`select * from index1 where type_id=6`,function(err,rows){
+       res.send({
+         data:rows,
+         status:200
+       })
+      })
+      })
+       // 首页 type_id=7
+    app.get('/index7', (req, res)=> {
+      connection.query(`select * from index1 where type_id=7`,function(err,rows){
+       res.send({
+         data:rows,
+         status:200
+       })
+      })
+      })
+       // 首页 type_id=8
+    app.get('/index8', (req, res)=> {
+      connection.query(`select * from index1 where type_id=8`,function(err,rows){
+       res.send({
+         data:rows,
+         status:200
+       })
+      })
+      })
+       // 首页 type_id=9
+    app.get('/index9', (req, res)=> {
+      connection.query(`select * from index1 where type_id=9`,function(err,rows){
+       res.send({
+         data:rows,
+         status:200
+       })
+      })
+      })
+         // 首页 type_id=10
+    app.get('/index10', (req, res)=> {
+      connection.query(`select * from index1 where type_id=10`,function(err,rows){
+       res.send({
+         data:rows,
+         status:200
+       })
+      })
+      })
+      // 首页列表狗狗
+      app.get('/index11', (req, res)=> {
+        var id=req.query.id
+        var tyid=req.query.tyid
+        console.log(id,tyid)
+        connection.query(`select * from nav_txt where type_id1=${id} and type_id=${tyid}`,function(err,rows){
+         res.send({
+           data:rows,
+           status:200
+         })
+        })
+      })
+     
 
 
 //  水族
--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,223 @@
-var mySwiper  = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    simulateTouch:false,
-    paginationClickable: true,
-    paginationBulletRender: function (swiper, index, className) {
-        return '<span class="' + className + '">' + (index + 1) + '</span>';
-    }
-});
-
-var span=document.querySelectorAll('.swiper-pagination span')
-var swiper=document.getElementById('swiper')
-for(var i=0;i<span.length;i++){
-    span[i].setAttribute("data-index",`${i}`)
-}
-span.forEach((item)=>{
-    item.onmouseover=function(e){
-        var indexto=e.target.dataset.index
-        mySwiper.slideTo(indexto, 1000, false);
+var content=document.querySelector('.content')
+var contentitem=document.querySelectorAll('.content-item')
+var span=document.querySelectorAll('.content-num span')
+span.forEach((item,index)=>{
+    item.onmouseover=function(){
+        for(var i=0;i<contentitem.length;i++){
+            contentitem[i].classList.remove('show')
+            
+        }
+        contentitem[index].classList.add('show')
+        for(var i=0;i<span.length;i++){
+            span[i].classList.remove('befor')
+        }
+        span[index].classList.add('befor')
     }
 })
+// 狗狗
+var span1=document.querySelectorAll('.dog-point span')
+var shuffling=document.querySelector('.dog-shuffling')
+// 猫咪
+var span2=document.querySelectorAll('.cat-point span')
+var shuffling1=document.querySelector('.cat-shuffling')
+// 小宠
+var span3=document.querySelectorAll('.pet-point span')
+var shuffling2=document.querySelector('.pet-shuffling')
+// 水族
+var span4=document.querySelectorAll('.animals-point span')
+var shuffling3=document.querySelector('.animals-shuffling')
+// 爬虫
+var span5=document.querySelectorAll('.crawler-point span')
+var shuffling4=document.querySelector('.crawler-shuffling')
+
+function lunbo(data,data1,data2){
+    var index=0
+    var mleft=0
+    data.forEach((item,cindex)=>{
+        item.onmouseover=function(){
+            for(var i=0;i<data.length;i++){
+                data[i].classList.remove(data2)
+            }
+            data[cindex].classList.add(data2)
+    
+            var timer=setInterval(function(){
+               if(cindex>index){
+                mleft+=60
+                if(mleft>=360*cindex){
+                    clearInterval(timer)
+                    index=cindex
+                }
+                data1.style.marginLeft=-mleft+'px'
+               }else if(cindex<index){
+                   mleft-=60
+                   if(mleft<=360*cindex){
+                    clearInterval(timer)
+                    index=cindex
+                }
+                data1.style.marginLeft=-mleft+'px'
+    
+               }
+           })
+        }
+    })
+}
+// 狗狗轮播图
+lunbo(span1,shuffling,'col')
+// 猫咪轮播图
+lunbo(span2,shuffling1,'col1')
+// 小宠轮播图
+lunbo(span3,shuffling2,'col2')
+// 水族轮播
+lunbo(span4,shuffling3,'col3')
+// 爬虫轮播
+lunbo(span5,shuffling4,'col4')
 
 
+var demo='index1';
+var demo1='index2';
+var demo2='index3';
+var demo3='index4';
+var demo4='index5';
+var demo5='index6';
+var demo6='index7';
+var demo7='index8';
+var demo8='index9';
+var demo9='index10';
+// 狗狗数据
+var dog=document.querySelector('.dog-r-c-l')
+function getdata(demo,type){
+    axios.get(`http://localhost:3000/${demo}`).then(res=>{
+        var str=''
+    
+        for(var i=0;i<res.data.data.length;i++){
+            str+=`
+            <a href="javascript:;">
+            <img src="${res.data.data[i].img}" alt="">
+            <p class="name">${res.data.data[i].title}</p>
+            <p>${res.data.data[i].cont}</p>
+        </a>
+            `
+        }
+        type.innerHTML=str
+    }).catch(err=>{
+        console.log(err)
+    })
+}
+getdata(demo,dog)
+var dog1=document.querySelector('.dog-r-b-c')
+function getdata1(demo1,type){
+    axios.get(`http://localhost:3000/${demo1}`).then(res=>{
+        var str=''
+      
+        for(var i=0;i<res.data.data.length;i++){
+            str+=`
+                    <a href="javascript:;">
+                        <dl>
+                            <dt>
+                                <img width="90" height="90"
+                                    src="${res.data.data[i].img}" alt="">
+                            </dt>
+                            <dd class="name">${res.data.data[i].title}</dd>
+                            <dd>${res.data.data[i].cont}</dd>
+                        </dl>
+                    </a>
+        
+            `
+        }
+        type.innerHTML=str
+    }).catch(err=>{
+        console.log(err)
+    })
+}
+getdata1(demo1,dog1)
+// 猫咪数据
+var cat=document.querySelector('.cat-r-c-l')
+getdata(demo2,cat)
+var cat1=document.querySelector('.cat-r-b-c')
+getdata1(demo3,cat1)
+
+// 小宠数据
+var pet=document.querySelector('.pet-r-c-l')
+getdata(demo4,pet)
+var pet1=document.querySelector('.pet-r-b-c')
+getdata1(demo5,pet1)
+
+// 水族数据
+var animals=document.querySelector('.animals-r-c-l')
+getdata(demo6,animals)
+var animals1=document.querySelector('.animals-r-b-c')
+getdata1(demo7,animals1)
+
+// 爬虫数据
+var crawler=document.querySelector('.crawler-r-c-l')
+getdata(demo8,crawler)
+var crawler1=document.querySelector('.crawler-r-b-c')
+getdata1(demo9,crawler1)
+
+
+
+
+// 渲染标题列表狗狗
+var dogdd=document.querySelectorAll('.content-t-dd')
+var dogdd1=document.querySelectorAll('.content-b-dd')
+function gettype(id=1,tyid=1,index=0){
+    axios.get(`http://localhost:3000/index11`,{
+        params:{
+            id,
+            tyid
+        }
+    }).then(res=>{
+       
+        var str=''
+        for(var i=0;i<res.data.data.length;i++){
+            str+=`
+            <a href="javascript:;">${res.data.data[i].cont}</a>
+        
+            `
+        }
+        dogdd[index].innerHTML=str
+    }).catch(err=>{
+        console.log(err)
+    })
+}
+function gettype1(id=1,tyid=2,index=0){
+    axios.get(`http://localhost:3000/index11`,{
+        params:{
+            id,
+            tyid
+        }
+    }).then(res=>{
+       
+        var str=''
+
+        for(var i=0;i<res.data.data.length;i++){
+            str+=`
+        
+            <a href="javascript:;">${res.data.data[i].cont}</a>
+            
+            `
+        }
+        dogdd1[index].innerHTML=str
+    }).catch(err=>{
+        console.log(err)
+    })
+}
+gettype()
+gettype1()
+var lists=document.querySelectorAll('.nav-item')
+var navcontent=document.querySelectorAll('.nav-content')
+lists.forEach((item,index)=>{
+    item.onmouseover=function(){
+        for(var i=0;i<navcontent.length;i++){
+            navcontent[i].classList.remove('ashow')
+        }
+        navcontent[index-1].classList.add('ashow')
+        gettype(id=index,tyid=(index*2)-1,index-1)
+        gettype1(id=index,typeid=index*2,index-1)  
+    }
+    item.onmouseout=function(){
+        for(var i=0;i<navcontent.length;i++){
+            navcontent[i].classList.remove('ashow')
+        }
+    }
+})
